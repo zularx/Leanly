c725692f79169db534cbf5fbebb3b32046ca85c9
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-07 01:32:29 +0300

    добавлен самый первый график на странице graphs

--- src/views/AppGraphs.vue
+++ src/views/AppGraphs.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
     import DefaultLayout from '@/layout/DefaultLayout.vue';
+    import type { WeightHistory } from '@/types/weightHistory';
+    import { ref, onMounted } from 'vue';
+    import { chartKey } from '@/keys/chartKey';
+    import DefaultSection from '@/components/ui/DefaultSection.vue';
+    import WeightChart from '@/components/charts/WeightChart.vue';
+    import { fetchWeightHistory } from '@/api/weightApi.ts';
+
+    const weights = ref<WeightHistory[]>([])
+
+    onMounted(async () => {
+        weights.value = await fetchWeightHistory()
+    })
 </script>
 <template>
     <DefaultLayout>
-        graphs page works!
+        <DefaultSection>
+            <template #h2Header>
+                График веса
+            </template>
+            <template #mainContent>
+                <WeightChart :weights="weights" :key="chartKey"></WeightChart>
+            </template>
+        </DefaultSection>
     </DefaultLayout>
 </template>
