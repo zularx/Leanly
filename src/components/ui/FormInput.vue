6c973e6632c97ec3cf562d8ba014e827501ee1af
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-07 00:43:27 +0300

    добавил кнопку просмотра пароля также исправил поведение bg input при autofill

--- src/components/ui/FormInput.vue
+++ src/components/ui/FormInput.vue
@@ -5,31 +5,40 @@
         :for="id">
             <slot name="inputLabel"></slot>
         </label>
+        <div class="relative w-full">
+            <input
+                class="p-2 w-full border-2 appearance-none border-[#cbcfc3] dark:bg-[#13203a] dark:border-[#13203a] bg-[#cbcfc3] hover:bg-[#afb2a8] dark:hover:bg-[#1e325b] dark:hover:border-[#1e325b] hover:border-[#afb2a8] focus:border-[#419400] dark:focus:border-[#3abdf7] user-invalid:border-[#f83a57] dark:user-invalid:border-[#fb7085] rounded-xl outline-0 transition-colors duration-100 md:text-xl lg:p-3"
+                :id="id"
+                :name="name"
+                :type="computedType"
+                :value="modelValue"
+                :placeholder="placeholder"
+                :required="required"
+                :min="min"
+                :max="max"
+                :pattern="pattern"
+                :step="step"
+                :minlength="minlength"
+                :maxlength="maxlength"
+                @input="onInput"
+                />
+    
+            <div v-if="props.type === 'password'" class="absolute right-3 top-0 h-full flex items-center">
+                <button class="bg-[#343300] hover:bg-[#bdc000] active:bg-[#419400] dark:bg-[#cdd0d4] dark:hover:bg-[#3abdf7] dark:active:bg-[#818cf8] duration-100 w-5 h-5 rounded-2xl cursor-pointer" type="button" @mousedown="showPassword = true" @mouseup="showPassword = false" @mouseleave="showPassword = false" @touchstart="showPassword = true" @touchend="showPassword = false" @touchcancel="showPassword = false">
 
-        <input
-            class="p-2 border-2 appearance-none border-[#cbcfc3] dark:bg-[#13203a] dark:border-[#13203a] bg-[#cbcfc3] hover:bg-[#afb2a8] dark:hover:bg-[#1e325b] dark:hover:border-[#1e325b] hover:border-[#afb2a8] focus:border-[#419400] dark:focus:border-[#3abdf7] user-invalid:border-[#f83a57] dark:user-invalid:border-[#fb7085] rounded-xl outline-0 transition-colors duration-100 md:text-xl lg:p-3"
-            :id="id"
-            :name="name"
-            :type="type"
-            :value="modelValue"
-            :placeholder="placeholder"
-            :required="required"
-            :min="min"
-            :max="max"
-            :pattern="pattern"
-            :step="step"
-            :minlength="minlength"
-            :maxlength="maxlength"
-            @input="onInput"
-            />
+                </button>
+            </div>
+        </div>
 
         <p class="text-[#fb7085] ml-2 my-2" v-if="$slots.inputError"><slot name="inputError"></slot></p>
-
     </div>
 </template>
 
 <script setup lang="ts">
-    defineProps<{
+import { computed, ref } from 'vue';
+
+
+    const props = defineProps<{
         modelValue?: string
         id?: string
         name?: string
@@ -43,6 +52,8 @@
         minlength?: number | string
         maxlength?: number | string
         }>()
+    
+    const showPassword = ref(false)
 
     const emit = defineEmits<{
         (e: 'update:modelValue', value: string): void
@@ -55,4 +66,13 @@
         emit('update:modelValue', input.value)
         emit('validation', input.validationMessage)
     }
+
+    const computedType = computed(() => {
+        if (props.type !== 'password') {
+            return props.type
+        }
+        
+        return showPassword.value ? 'text' : 'password';
+    })
 </script>
+
--- src/views/register/RegStep1.vue
+++ src/views/register/RegStep1.vue
@@ -7,6 +7,7 @@
     import FormHeader from '@/components/ui/FormHeader.vue';
     import { ref } from 'vue';
 
+
     const router = useRouter();
 
     const register = useRegisterStore();
@@ -26,19 +27,43 @@
             <FormHeader>Регистрация: шаг 1</FormHeader>
             <form @submit.prevent="nextStep" class="w-full flex mt-4 flex-col gap-3 items-center justify-center text-[#151614]">
                 <FormInput v-model="register.nickname" id="nickName" name="nickName" type="text" placeholder="Введите никнейм" minlength="2" maxlength="30" required>
-                    Никнейм
+                    <template #inputLabel>
+                        Никнейм
+                    </template>
+
+                    <template #inputError>
+                        Заглушка ошибки
+                    </template>
                 </FormInput>
 
                 <FormInput v-model="register.email" id="email" name="email" type="email" placeholder="Введите email" maxlength="100" required>
-                    Электронная почта
+                    <template #inputLabel>
+                        Электронная почта
+                    </template>
+
+                    <template #inputError>
+                        Заглушка ошибки
+                    </template>
                 </FormInput>
 
                 <FormInput v-model="register.password" id="password" name="password" type="password" placeholder="Введите пароль" minlength="10" maxlength="128" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,128}$" required>
-                    Пароль
+                    <template #inputLabel>
+                        Пароль
+                    </template>
+
+                    <template #inputError>
+                        Заглушка ошибки
+                    </template>
                 </FormInput>
 
                 <FormInput v-model="confirmPassword" id="confirmPassword" name="confirmPassword" type="password" placeholder="Введите пароль" minlength="10" maxlength="128" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,128}$" required>
-                    Подтвердите пароль
+                    <template #inputLabel>
+                        Подтвердите пароль
+                    </template>
+
+                    <template #inputError>
+                        Заглушка ошибки
+                    </template>
                 </FormInput>
 
                 <NextStepBtn>
