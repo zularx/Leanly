2418fe84a0a41b2b2ac0faf0282233d4654745b7
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-07 05:08:35 +0300

    добавил фидбек ссылку

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+
+
+</script>
+
 <template>
-    <footer class="flex h-40 bg-[#f8fdef] dark:bg-[#0a1120] rounded-2xl justify-center items-center">
+    <footer class="flex h-40 bg-[#f8fdef] dark:bg-[#0a1120] rounded-2xl justify-center items-center gap-5">
         <p class="dark:text-white font-semibold md:text-xl">© 2026 Leanly. Все права защищены.</p>
+        <a href="https://github.com/zularx/Leanly/issues/new" class="dark:text-[#3abdf7] text-[#419400] dark:hover:text-[#59ccfe] hover:text-[#53be00] dark:active:text-[#0ab4fc] active:text-[#4bae00]">Предложить улучшение проекта.</a>
     </footer>
 </template>
